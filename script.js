1bd94ab6a4a4a871f3533e2e89b1869d95a2ee74
islam sani <islammuhd8@gmail.com>
2023-10-11 15:37:16 +0100

    converting human years to dig's

---
+++ script.js
@@ -0,0 +1,32 @@
+// declaring myAge vriable
+let myAge = 28;
+
+// declaring earlyYears vriable
+let earlyYears = 2;
+
+
+earlyYears *= 10.5;
+
+// take the myAge variable, and subtract 2 from it
+let latertYears = myAge - 2
+
+//multiplying latertYears by 4 to calculate the num of dog's years accounted for by your latert Years
+latertYears *= 4;
+
+//checking the values
+console.log(earlyYears, latertYears);
+
+// Adding earlyYears and laterYears together
+myAgeInDogYears = earlyYears + latertYears;
+
+//declaring myName variable and adding a method
+let myName = 'Islam Sani'.toLocaleLowerCase();
+
+// printing a statement that displays name and age in dog years. using  string interpolation. 
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+
+
+
+
+
+
